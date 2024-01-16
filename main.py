4eebac2fe3dfec6972d4da8a86e89839afc651e2
tcabgom <tcabgom222@gmail.com>
2024-01-16 12:17:22 +0100

    Refactor code to allow easier implementations and different environments

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 import gymnasium
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, DQN
 import os
 from stable_baselines3.common.callbacks import BaseCallback
-from gymnasium.wrappers import GrayScaleObservation, ResizeObservation, FrameStack, FlattenObservation, AtariPreprocessing
+from gymnasium.wrappers import GrayScaleObservation, ResizeObservation
 from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
 
 
@@ -13,12 +13,16 @@ CHECKPOINT_DIR = "./train/"
 LOG_DIR = "./logs/"
 
 
-def test_random_actions_tutorial():
+def test_random_actions_tutorial(env):
+    """
+    Renders the given environment performing random actions.
 
-    env = gym_super_mario_bros.make('SuperMarioBros-v3', render_mode="human")
+    Args:
+        env: The environment that will be used
 
-    env = reduce_action_space(env)
-    env = reduce_observation_space(env)
+    Returns:
+        None
+    """
     print_environment_data(env)
 
     terminated = True
@@ -50,7 +54,19 @@ class TrainAndLoggingCallback(BaseCallback):
         return True
 
 
-def train_agent():
+def train_agent(env, model, check_freq, total_timesteps):
+    """
+    Trains the given environment with the given model
+
+    Args:
+        env: The environment that will be used to train
+        model: The model that will be trained
+        check_freq: The number of iterations that will run before saving a copy of the model
+        total_timesteps: The number of iterations of the training
+
+    Returns:
+        model: The model after all iterations of the training
+    """
     env = gym_super_mario_bros.make('SuperMarioBros-v0', render_mode=None)
     print_environment_data(env)
 
@@ -60,44 +76,26 @@ def train_agent():
 
     print_environment_data(env)
 
-    callback = TrainAndLoggingCallback(check_freq=100000, save_path=CHECKPOINT_DIR)
+    callback = TrainAndLoggingCallback(check_freq=check_freq, save_path=CHECKPOINT_DIR)
 
     model = PPO("CnnPolicy", env, verbose=1, tensorboard_log=LOG_DIR, learning_rate=0.000001, n_steps=512)
 
-    model.learn(total_timesteps=4000000, callback=callback)
+    model.learn(total_timesteps=total_timesteps, callback=callback)
 
     return model
 
 
+def load_and_test_model(env, model_path):
+    """
+    Loads a trained model of the environment to test its performance
+    """
 
-class FrameSkipWrapper(gymnasium.Wrapper):
-    def __init__(self, env, skip):
-        super(FrameSkipWrapper, self).__init__(env)
-        self.skip = skip
-
-    def step(self, action):
-        total_reward = 0.0
-        done = False
-        for i in range(self.skip):
-            observation, reward, terminated, truncated, info = self.env.step(action)
-            total_reward += reward
-            done = done or terminated
-            if done:
-                break
-        return observation, total_reward, done, truncated, info
-
-
-
-
-def load_and_test_model():
-
-    env = gym_super_mario_bros.make('SuperMarioBros-v0', render_mode="human")
     env = reduce_action_space(env)
     env = reduce_observation_space(env)
 
     terminated = True
     truncated = False
-    model = PPO.load('./train/best_model_8400000', env=env)
+    model = PPO.load(model_path, env=env)
     vec_env = model.get_env()
     observation = vec_env.reset()
     for step in range(5000):
@@ -121,21 +119,36 @@ def reduce_action_space(env):
 
 def reduce_observation_space(env):
     # https://gymnasium.farama.org/api/wrappers/
-    env = GrayScaleObservation(env, keep_dim=True) # Convertimos la imagen en blanco y negro
-    env = ResizeObservation(env, shape=(60, 64))   # Reducimos el tamaño de la imagen al 25%
-    #env = DummyVecEnv([lambda: env])
-    #env = VecFrameStack(env, n_stack=4)
+    env = GrayScaleObservation(env, keep_dim=True) # Convert the image to black and white
+    env = ResizeObservation(env, shape=(60, 64))   # Reduce the image size
 
     return env
 
 
 def enhance_observation_space(env):
-    #env = FrameStack(env, num_stack=3)
+    env = DummyVecEnv([lambda: env])     #
+    env = VecFrameStack(env, n_stack=4)  # Stacking frames to let the model recognize speed
     return env
 
 
 if __name__ == '__main__':
-    #load_and_test_model()
-    #test_random_actions_tutorial()
-    m = train_agent()
+    env = gymnasium.make("ALE/SpaceInvaders-v5", render_mode='rgb_array')
+    #env = gym_super_mario_bros.make('SuperMarioBros-v3', render_mode="human")
+    model = DQN("CnnPolicy", env,
+                verbose=1,                    # Controls the verbosity level (0: no output, 1: training information)
+                tensorboard_log=LOG_DIR,      # Directory for storing Tensorboard logs
+                learning_rate=0.00001,        # The learning rate for the optimizer
+                buffer_size=50000,            # Size of the replay buffer
+                learning_starts=1000,         # Number of steps before starting to update the model
+                train_freq=4,                 # Number of steps between updates of the model
+                gradient_steps=1,             # Number of gradient steps to take per update
+                target_update_interval=10000, # Update target network every `target_update_interval` steps
+                exploration_fraction=0.05,    # Fraction of total timesteps during which exploration rate is decreased
+                exploration_final_eps=0.01,   # Final value of the exploration rate
+                max_grad_norm=10,             # Clipping of gradients during optimization
+                gamma=0.99                    # Discount factor for future rewards
+                )
+    #test_random_actions_tutorial(env)
+    #load_and_test_model(env, "./train/best_model_8400000")
+    m = train_agent(env, model, 100000, 4000000)
     print('Model trained')
