2c7f2151e0a179512b1ce6c5779a565cbc481f29
tcabgom <tcabgom222@gmail.com>
2024-04-27 20:05:47 +0200

    Removed ubused fuction

--- src/PPO_model.py
+++ src/PPO_model.py
@@ -9,16 +9,16 @@ def create_PPO_model(env) -> PPO:
                 verbose=1,                                  # Controls the verbosity level (0: no output, 1: training information)
                 tensorboard_log=experiment_utils.LOG_DIR,   # Directory for storing Tensorboard logs
                 learning_rate=0.00025,                     # The learning rate for the optimizer
-                gamma=0.99,                                 # Discount factor for future rewards
-                gae_lambda=0.95,                            # Lambda for the Generalized Advantage Estimator
-                max_grad_norm=0.5,                          # Clipping of gradients during optimization
-                n_steps=128,                                # Number of steps to run for each environment per update
-                n_epochs=4,                                 # Number of epochs when optimizing the surrogate loss
-                batch_size=256,                             # Minibatch size for each environment
-                clip_range=0.1,                             # Clipping parameter, mainly used for PPO
-                ent_coef=0.01,                              # Entropy coefficient for the loss calculation
-                vf_coef=0.5,                                # Value function coefficient for the loss calculation
-                policy_kwargs=dict(optimizer_class=RMSpropTFLike, optimizer_kwargs=dict(eps=1e-5))     # Additional network architecture
+                #gamma=0.99,                                 # Discount factor for future rewards
+                #gae_lambda=0.95,                            # Lambda for the Generalized Advantage Estimator
+                #max_grad_norm=0.5,                          # Clipping of gradients during optimization
+                n_steps=2048,                                # Number of steps to run for each environment per update
+                #n_epochs=4,                                 # Number of epochs when optimizing the surrogate loss
+                #batch_size=256,                             # Minibatch size for each environment
+                #clip_range=0.1,                             # Clipping parameter, mainly used for PPO
+                #ent_coef=0.01,                              # Entropy coefficient for the loss calculation
+                #vf_coef=0.5,                                # Value function coefficient for the loss calculation
+                #policy_kwargs=dict(optimizer_class=RMSpropTFLike, optimizer_kwargs=dict(eps=1e-5))     # Additional network architecture
                 # device = "cuda:0"
                 )
 
